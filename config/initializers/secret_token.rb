1dca4af6cde08dd053cf7ca9a3c236897cb9f30f
Peet Gouws <Peet@Gouws.com>
2014-03-16 14:09:22 +0200

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoinWatcher::Application.config.secret_key_base = 'd0d29888c3474d023609f06778546cf150adb855b2127c58c78fa7d6a68709939d07f16aa261b7ee53861b88c4f4453f34e1dc77c70d427232c4ced92dc2a7e2'
